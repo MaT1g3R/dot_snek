1db3398c8a3a69759b8a14ce927b20d9ac927033
Umi <982341777@qq.com>
2017-05-29 19:29:01 -0400

    0.0.1 release

--- snek/main.py
+++ snek/main.py
@@ -5,10 +5,13 @@ from sys import argv
 from snek.core import link_all
 from snek.update import update
 
-if __name__ == '__main__':
-    if argv[1].lower() == 'update':
+
+def main():
+    if len(argv) < 2:
+        pass
+    elif argv[1].lower() == 'update':
         update()
-    if argv[1] == 'link':
+    elif argv[1] == 'link':
         environments = argv[2:]
         for e in environments:
             link_all(Path(f'../{e}').resolve(), Path(expanduser('~')))
--- snek/update.py
+++ snek/update.py
@@ -1,6 +1,3 @@
-from os import chdir, getcwd
-from pathlib import Path
-
 from snek.core import shell_command
 
 
@@ -8,11 +5,8 @@ def update():
     """
     Pull all changes from the git repo to local
     """
-    old_dir = getcwd()
-    chdir(Path('..').absolute())
     shell_command('git submodule update --recursive --remote')
     shell_command('git add .')
     shell_command('git commit -m "Update"')
     shell_command('git pull')
     shell_command('git push')
-    chdir(old_dir)
