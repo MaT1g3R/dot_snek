e6df957b91195b67862dc9b9a24180ca82756de7
Umi <982341777@qq.com>
2017-05-29 19:54:32 -0400

    v0.0.3

--- snek/main.py
+++ snek/main.py
@@ -10,7 +10,7 @@ from snek.update import update
 def main():
     if len(argv) < 2:
         return
-    chdir(list(shell_command('echo $PWD', False))[0])
+    chdir(list(shell_command('echo $PWD', False))[0].strip('\n'))
     if argv[1].lower() == 'update':
         update()
     elif argv[1] == 'link':
