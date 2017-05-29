26c9567e93a2ad13477b5ad92ad129dc34f43389
Umi <982341777@qq.com>
2017-05-28 23:58:18 -0400

    snek

--- snek/core.py
+++ snek/core.py
@@ -1,4 +1,5 @@
 from codecs import decode
+from os import link
 from pathlib import Path
 from subprocess import Popen, PIPE, STDOUT, call
 
@@ -31,7 +32,7 @@ def __base_cond(path: Path):
     :param path: the base file/dir path
     :return: True if the base file/dir name passed the base condition check
     """
-    return path.name not in (
+    return path.name.lower() not in (
         '.git', '.gitignore', '.gitkeep', '.directory', '.gitmodules',
         '.github', '.travis.yml'
     )
@@ -97,9 +98,9 @@ def shell_command(cmd: str):
         print(decode(line))
 
 
-def create_symlink(src: Path, dest: Path):
+def create_link(src: Path, dest: Path):
     """
-    Create a symlink from the src file to the dest directory
+    Create a link from the src file to the dest directory
 
     :param src: the src file path
 
@@ -112,20 +113,20 @@ def create_symlink(src: Path, dest: Path):
     assert not dest.is_dir()
     if dest.exists():
         dest.unlink()
-    dest.symlink_to(src)
+    link(src.absolute(), dest.absolute())
     dest.resolve()
 
 
 def __link_all(src: Path, dest: Path):
     """
-    Recursively symlink all files under a the root path to the dest path
+    Recursively link all files under a the root path to the dest path
     :param src: the source path
     :param dest: the dest path
     """
     if __base_cond(src):
         if src.is_file():
             print('Linking {} to {}'.format(src, dest))
-            create_symlink(src, dest)
+            create_link(src, dest)
         elif src.is_dir():
             for sub in src.iterdir():
                 __link_all(src.joinpath(sub), dest.joinpath(sub.name))
