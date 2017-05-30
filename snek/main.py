16269c17b3b31c932c27502686e9ab21d2073af4
Umi <982341777@qq.com>
2017-05-29 20:02:20 -0400

    0.0.4

--- snek/main.py
+++ snek/main.py
@@ -10,15 +10,10 @@ from snek.update import update
 def main():
     if len(argv) < 2:
         return
-    chdir(list(shell_command('echo $PWD', False))[0].strip('\n'))
+    chdir(shell_command('echo $PWD', False)[0].strip('\n'))
     if argv[1].lower() == 'update':
         update()
     elif argv[1] == 'link':
         environments = argv[2:]
         for e in environments:
             link_all(Path(f'./{e}').resolve(), Path(expanduser('~')))
-
-
-if __name__ == '__main__':
-    chdir('..')
-    update()
